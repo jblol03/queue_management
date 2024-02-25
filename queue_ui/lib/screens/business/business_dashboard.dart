426f243cc654eae68a9fea1d1d258065e84e2d71
pritam-ravani <21amtics272@gmail.com>
2024-02-25 18:05:03 +0530

    Update

--- queue_ui/lib/screens/business/business_dashboard.dart
+++ queue_ui/lib/screens/business/business_dashboard.dart
@@ -7,82 +7,79 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.1, // 10% of screen width
-        height: MediaQuery.of(context).size.width * 0.05, // 5% of screen width
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Queue Management System'),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.account_circle),
-                onPressed: () {
-                  // Navigate to user profile
+      // 5% of screen width
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Queue Management System'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.account_circle),
+              onPressed: () {
+                // Navigate to user profile
+              },
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: 20, // Replace this with the length of your data list
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text((index + 1).toString()), // Appointment number
+              title: const Text('Jay Billimoria'), // Appointment name
+              trailing: PopupMenuButton(
+                itemBuilder: (context) => [
+                  const PopupMenuItem(
+                    value: 1,
+                    child: Text('Remove'),
+                  ),
+                  const PopupMenuItem(
+                    value: 2,
+                    child: Text('Done'),
+                  ),
+                  const PopupMenuItem(
+                    value: 3,
+                    child: Text('Re-call'),
+                  ),
+                ],
+                onSelected: (value) {
+                  // Handle menu option selection
                 },
               ),
-            ],
-          ),
-          body: ListView.builder(
-            itemCount: 20, // Replace this with the length of your data list
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: Text((index + 1).toString()), // Appointment number
-                title: const Text('Jay Billimoria'), // Appointment name
-                trailing: PopupMenuButton(
-                  itemBuilder: (context) => [
-                    const PopupMenuItem(
-                      value: 1,
-                      child: Text('Remove'),
-                    ),
-                    const PopupMenuItem(
-                      value: 2,
-                      child: Text('Done'),
-                    ),
-                    const PopupMenuItem(
-                      value: 3,
-                      child: Text('Re-call'),
-                    ),
-                  ],
-                  onSelected: (value) {
-                    // Handle menu option selection
-                  },
-                ),
-              );
-            },
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Dashboard',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Explore',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications),
-                label: 'Notifications',
-              ),
-            ],
-            onTap: (index) {
-              // Handle navigation
-              switch (index) {
-                case 0:
-                  // Navigate to Dashboard
-                  break;
-                case 1:
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ExplorePage()),
-                  );
-                  break;
-                case 2:
-                  // Navigate to Notifications
-                  break;
-              }
-            },
-          ),
+            );
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Dashboard',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Explore',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'Notifications',
+            ),
+          ],
+          onTap: (index) {
+            // Handle navigation
+            switch (index) {
+              case 0:
+                // Navigate to Dashboard
+                break;
+              case 1:
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ExplorePage()),
+                );
+                break;
+              case 2:
+                // Navigate to Notifications
+                break;
+            }
+          },
         ),
       ),
     );
